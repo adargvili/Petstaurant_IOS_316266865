ff7234869f9f4b4d2d18ae01a6de14383964bcf8
adar <adar555@gmail.com>
2022-05-29 19:01:45 +0300

    User Model Init

---
+++ Petstaurant_IOS_316266865/Model/User.swift
@@ -0,0 +1,46 @@
+//
+//  User.swift
+//  Petstaurant_IOS_316266865
+//
+//  Created by Dror Bank on 29/05/2022.
+//
+
+import Foundation
+
+class User{
+    var id : String?
+    var email : String?
+    var userName : String?
+    var profileImageUrl : String?
+    
+    init(){}
+    
+    init(id:String,email:String,userName:String,profileImageUrl:String){
+        self.id = id
+        self.email = email
+        self.userName = userName
+        self.profileImageUrl = profileImageUrl
+    }
+    
+    
+}
+
+extension User{
+    static func FromJson(json:[String:Any])->User{
+        let s = User()
+        s.id = json["id"] as? String
+        s.email = json["email"] as? String
+        s.userName = json["userName"] as? String
+        s.profileImageUrl = json["image"] as? String
+        return s
+    }
+    
+    func toJson()->[String:Any]{
+        var json = [String:Any]()
+        json["id"] = self.id!
+        json["email"] = self.email!
+        json["userName"] = self.userName!
+        json["image"] = self.profileImageUrl!
+        return json
+    }
+}
