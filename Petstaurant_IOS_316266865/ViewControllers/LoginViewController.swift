532abbd13e9fa556289dcc6cdccebd16dbb264c4
adar <adar555@gmail.com>
2022-05-29 21:11:34 +0300

    started auth

---
+++ Petstaurant_IOS_316266865/ViewControllers/LoginViewController.swift
@@ -0,0 +1,15 @@
+//
+//  LoginViewController.swift
+//  Petstaurant_IOS_316266865
+//
+//  Created by Dror Bank on 29/05/2022.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ Petstaurant_IOS_316266865/ViewControllers/SignUpViewController.swift
@@ -0,0 +1,15 @@
+//
+//  SignUpViewController.swift
+//  Petstaurant_IOS_316266865
+//
+//  Created by Dror Bank on 29/05/2022.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
