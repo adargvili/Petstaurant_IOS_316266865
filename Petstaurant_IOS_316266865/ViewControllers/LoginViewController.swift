905da76ff9d926d2c2a926b87f7f415414764aa5
adar <adar555@gmail.com>
2022-06-03 11:44:15 +0300

    signing validations

--- Petstaurant_IOS_316266865/ViewControllers/LoginViewController.swift
+++ Petstaurant_IOS_316266865/ViewControllers/LoginViewController.swift
@@ -23,6 +23,19 @@ class LoginViewController: UIViewController {
     
     
     @IBAction func loginTapped(_ sender: Any) {
+        
+        var fields = [String]()
+        fields.append(emailLoginTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines))
+        fields.append(passwordLoginTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines))
+        
+        
+        if !Model.instance.validateEmptyFields(fields: fields){errorLoginBtn.text = "At least one of the fields is empty";return;}
+        
+        if !Model.instance.validateEmail(email: emailLoginTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)){errorLoginBtn.text = "Email is not valid";return;}
+        
+        if !Model.instance.validatePassword(password: passwordLoginTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)){errorLoginBtn.text = "Minimum 8 and Maximum 10 characters at least 1 Uppercase Alphabet, 1 Lowercase Alphabet, 1 Number and 1 Special Character";return;}
+        
+        
         Model.instance.loginUser(email: emailLoginTextField.text!, password: passwordLoginTextField.text!){}
         
         let storyBoard : UIStoryboard = UIStoryboard(name: "Main", bundle:nil)
