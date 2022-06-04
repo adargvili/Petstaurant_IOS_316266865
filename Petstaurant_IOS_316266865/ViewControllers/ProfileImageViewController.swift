e94aa5e83ea2ef28aa60f5c0f5cafa545684d449
adar <adar555@gmail.com>
2022-06-04 11:56:32 +0300

    profileImage redirects updates

--- Petstaurant_IOS_316266865/ViewControllers/ProfileImageViewController.swift
+++ Petstaurant_IOS_316266865/ViewControllers/ProfileImageViewController.swift
@@ -9,9 +9,21 @@ import UIKit
 
 class ProfileImageViewController: UIViewController {
 
+    @IBOutlet weak var porfieImageAvatar: UIImageView!
+    @IBOutlet weak var profileGalleryImage: UIButton!
+    @IBOutlet weak var profileCameraImage: UIButton!
+    @IBOutlet weak var continueBtn: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-
+    @IBAction func continueBtnTapped(_ sender: Any) {
+        
+        let storyBoard : UIStoryboard = UIStoryboard(name: "Main", bundle:nil)
+        let resultViewController = storyBoard.instantiateViewController(withIdentifier: "viewController") as! ViewController
+        self.navigationController?.pushViewController(resultViewController, animated: true)
+        return
+    }
+    
 }
