ad55842ca5d83b425d307677f48e0552187ee566
adar <adar555@gmail.com>
2022-06-22 23:58:47 +0300

    about page design

--- Petstaurant_IOS_316266865/ViewControllers/AboutViewController.swift
+++ Petstaurant_IOS_316266865/ViewControllers/AboutViewController.swift
@@ -6,10 +6,12 @@
 //
 
 import UIKit
+import AVFoundation
 
 class AboutViewController: UIViewController {
 
-
+    var videoPlayer:AVPlayer?
+    var videoPlayerLayer:AVPlayerLayer?
     @IBOutlet var aboutLabel: UITextView!
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,4 +22,26 @@ class AboutViewController: UIViewController {
     }
 
 
+    override func viewWillAppear(_ animated: Bool) {
+        setUpVideo()
+    }
+    
+    
+    func setUpVideo(){
+        let bundlePath = Bundle.main.path(forResource: "DogReviews", ofType: "mp4")
+        guard bundlePath != nil else{
+            return
+        }
+        let url   = URL(fileURLWithPath: bundlePath!)
+        let item = AVPlayerItem(url: url)
+        videoPlayer = AVPlayer(playerItem: item)
+        videoPlayerLayer = AVPlayerLayer(player:videoPlayer!)
+        videoPlayerLayer?.frame = CGRect(x: -self.view.frame.size.width*1.5,
+                                         y:0,
+                                         width: self.view.frame.size.width*4,
+                                         height: self.view.frame.size.height)
+        view.layer.insertSublayer(videoPlayerLayer!, at: 0)
+        videoPlayer?.isMuted = true
+        videoPlayer?.playImmediately(atRate: 0.3)
+    }
 }
